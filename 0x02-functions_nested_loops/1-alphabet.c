fd64dd44c5d3867adf84cdc0910022ea5485e337
Cutie <berylwawuda81@gmail.com>
2022-06-08 09:19:48 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet a-z afer looping
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
