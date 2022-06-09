34c8a0bf175334153ea99618d970e8eb1267df1e
Cutie <berylwawuda81@gmail.com>
2022-06-09 06:27:00 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal lines
+ * @n: number of time '\' should be printed
+ * the program - "prints diagonal lines
+ */
+
+
+
+void print_diagonal(int n)
+
+{
+
+	int c, i;
+
+
+
+	c = 0;
+
+
+
+	while (n > 0)
+
+	{
+
+		_putchar(' ');
+
+		i--;
+
+	}
+
+	if (c < 1)
+
+		_putchar('\n');
+
+}
