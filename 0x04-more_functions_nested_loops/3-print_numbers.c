2eb3fab72b42581d3d9ede6b3b613cf82a3abfd4
Cutie <berylwawuda81@gmail.com>
2022-06-09 06:09:13 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints digit 0-9
+ * the program - "prints digit 0-9
+ */
+
+void print_numbers(void)
+
+{
+
+	int i;
+
+
+
+	for (i = 48; i <= 57; i++)
+
+	{
+
+		_putchar(i);
+
+	}
+
+	_putchar('\n');
+
+}
