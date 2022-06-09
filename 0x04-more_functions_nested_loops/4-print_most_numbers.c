28334a4df42f108a0d2a89957e61e55b25775ca6
Cutie <berylwawuda81@gmail.com>
2022-06-09 06:13:24 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints digit 0-9
+ * the program - "prints digit 0-9
+ * Return: void
+ */
+
+void print_most_numbers(void)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i <= 9; i++)
+
+	{
+
+		if (i != 2 && i != 4)
+
+		{
+
+			_putchar(i + '0');
+
+		}
+
+	}
+
+	_putchar('\n');
+
+}
