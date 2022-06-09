533208d3fa36f87b6ab49ee397b94c09aba33703
Cutie <berylwawuda81@gmail.com>
2022-06-09 06:57:52 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,18 +23,18 @@ int main(void)
 	while (i <= 100)
 
 	{
-		if (i % 3 == 0 && i % 5 == 0
-				printf("FizzBuzz");
-				else if (i % 3 == 0)
-				printf("Fizz");
-				else if (i % 5 == 0)
-				printf("Buzz");
-				else
-				printf("%d", i);
-				if (i != 100)
-				printf(" ");
-				i++;
-				}
-				printf("\n");
-				return (0);
-				}
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
