e486b9fe58ff870708f437fb9b4caf83c23a5b9b
Cutie <berylwawuda81@gmail.com>
2022-06-22 09:47:02 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: base.
- * y: exponent.
+ * @y: exponent.
  * Return: value of the exponentiation.
  */
 int _pow_recursion(int x, int y)
