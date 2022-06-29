f5394052fddfa98a552c1e63e13686c7e93e30ed
Cutie <berylwawuda81@gmail.com>
2022-06-29 06:53:01 -0700

    task 4 \

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: an array consisting arguments
+ *
+ * Return: 0 or 1
+ */
+
+int main(int argc, char **argv)
+{
+	int add = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
+
